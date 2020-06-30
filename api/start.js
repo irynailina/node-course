382c736c17954ec5f7b5da5f434cecc09d0702be
irynailina <irynailina@gmail.com>
2020-06-30 13:29:51 +0300

    deleted unusable code and logic from server.js replaced in index.js

--- api/start.js
+++ api/start.js
@@ -1,3 +1,3 @@
-const ContactsServer = require("./server");
+const ContactsServer = require("../index");
 
 new ContactsServer().start();
--- index.js
+++ index.js
@@ -1,74 +1,38 @@
-const argv = require("yargs").argv;
 const express = require("express");
-const Joi = require("@hapi/joi");
-const uuid = require("uuid");
-const morgan = require('morgan')
-const PORT = 3000;
+const cors = require("cors");
+require("dotenv").config();
+const contactRouter = require("./api/contacts/contacts.router");
+const morgan = require("morgan");
 
-const {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-} = require("./contacts.js");
-// const { require } = require("yargs");
-
-function invokeAction({ action, id, name, email, phone }) {
-  switch (action) {
-    case "list":
-      listContacts();
-      break;
-
-    case "get":
-      getContactById(id);
-      break;
-
-    case "add":
-      addContact(name, email, phone);
-      break;
-
-    case "remove":
-      removeContact(id);
-      break;
-
-    default:
-      console.warn("\x1B[31m Unknown action type!");
+module.exports = class ContactsServer {
+  constructor() {
+    this.server = null;
   }
-}
 
-invokeAction(argv);
-
-// ===================================
-
-const app = express();
-
-app.use(express.json());
-app.use(morgan('tiny'))
-
-function validateCreateUser(req, res, next) {
-  const userSchema = Joi.object({
-    name: Joi.string().required(),
-    email: Joi.string().required(),
-    phone: Joi.string().required(),
-  });
-
-  const result = userSchema.validate(req.body);
-  if (result.error) {
-    return res.status(400).json(result.error);
+  start() {
+    this.initServer();
+    this.initMiddlewares();
+    this.initRoutes();
+    this.startListening();
   }
-  next();
-}
 
-app.post("/contacts", validateCreateUser, (req, res, next) => {
- const id = uuid.v4();
- 
-  console.log(id);
-  return res.send("hello");
-});
+  initServer() {
+    this.server = express();
+  }
 
-app.listen(PORT, () => {
-  console.log("Server started listening on port", PORT);
-});
+  initMiddlewares() {
+    this.server.use(express.json());
+    this.server.use(morgan("tiny"));
+    this.server.use(cors({ origin: "http://localhost:3000" }));
+  }
 
+  initRoutes() {
+    this.server.use("/", contactRouter);
+  }
 
-app.get('/contacts', listContacts)
+  startListening() {
+    this.server.listen(process.env.PORT, () => {
+      console.log("Server started listening on port", process.env.PORT);
+    });
+  }
+};
